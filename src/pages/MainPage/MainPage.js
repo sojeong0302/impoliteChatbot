2407d3766ac27b89ef7a4cc0aa10e96e5c0e740d
sojeong0302 <01duqrl@naver.com>
2024-07-28 20:52:35 +0900

    design: 메인 레이아웃 #5

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,5 +1,29 @@
+import { MainContainer, MainAmong, AmongText, SwitchContainer, SwitchText } from "./MainPage.style.js";
+import { Button, Switch } from "@mui/material";
+
 const MainPage = () => {
-    return <></>;
+    return (
+        <>
+            <MainContainer>
+                <SwitchContainer>
+                    <SwitchText>Dark</SwitchText>
+                    <Switch />
+                    <SwitchText>Light</SwitchText>
+                </SwitchContainer>
+                <MainAmong>
+                    <AmongText>IMPOLITE CHATBOT</AmongText>
+                    <Button
+                        variant='contained'
+                        sx={{
+                            fontSize: "1.5rem",
+                        }}
+                    >
+                        채팅시작 ▶
+                    </Button>
+                </MainAmong>
+            </MainContainer>
+        </>
+    );
 };
 
 export default MainPage;
